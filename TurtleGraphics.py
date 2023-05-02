a3693a64cc93526315c4359e5f11f59a9660c827
Rimzhanovich617 <87210820+Rimzhanovich617@users.noreply.github.com>
2023-05-01 19:32:44 -0600

    Add files via upload

---
+++ TurtleGraphics.py
@@ -0,0 +1,10 @@
+import turtle
+
+from turtle import Turtle, Screen
+timmy.shapesize(3,3,6)
+timmy.color("cyan3")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
